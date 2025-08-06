869245654efeac2788cb14ee93bf19ea417cbadf
mnaboo <maciek@nabozny.pl>
2025-08-06 23:42:05 +0200

    CORS works

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,19 @@
 import express from "express";
 import loginRouter from "./routes/loginRouter";
-import signUpRouter from "./routes/signUpRouter"
+import signUpRouter from "./routes/signUpRouter";
 import cors from "cors";
 import { setupSwagger } from './config/swagger';
 
 const app = express();
 
-setupSwagger(app);
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true,
+}));
 
-app.use(cors());
 app.use(express.json());
+
+setupSwagger(app);
 
 app.use("/api/v1/signup", signUpRouter);
 app.use("/api/v1/login", loginRouter);
