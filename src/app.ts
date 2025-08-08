2494476aca42e105ba00f8b2403f4187657f2290
mnaboo <maciek@nabozny.pl>
2025-08-08 18:15:18 +0200

    port added for cors, gitignore and dockerignore changed (.idea), morgan added

--- src/app.ts
+++ src/app.ts
@@ -2,16 +2,26 @@ import express from "express";
 import loginRouter from "./routes/loginRouter";
 import signUpRouter from "./routes/signUpRouter";
 import cors from "cors";
+import morgan from "morgan";
 import { setupSwagger } from './config/swagger';
 
 const app = express();
 
+const allowedOrigins = ['http://localhost:3000', 'http://localhost:3001'];
+
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
 }));
 
 app.use(express.json());
+app.use(morgan('dev'));
 
 setupSwagger(app);
 
